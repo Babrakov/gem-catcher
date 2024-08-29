eeac692e61b24ed6415b4e592a689a6886578169
Sergei Babrakov <s.a.babrakov@gmail.com>
2024-08-29 21:34:39 +0200

    Custom signal

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,8 +1,5 @@
-extends Area2D
+extends Node2D
 
-@export var speed: float = 100.0
-
-signal on_gem_off_screen
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += speed * delta
-	
-	if position.y > get_viewport_rect().size.y:
-		on_gem_off_screen.emit()
-		set_process(false)
-		queue_free()
+	pass
+
+
+func _on_gem_off_screen() -> void:
+	print("gem off the screen")
