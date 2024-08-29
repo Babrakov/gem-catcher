02ac4a2f2e60189e2b42eb2ab693fcc70d9a97b8
Sergei Babrakov <s.a.babrakov@gmail.com>
2024-08-29 21:12:57 +0200

    Moving the paddle

---
+++ scenes/paddle.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var speed: float = 200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		position.x -= speed * delta
+	elif Input.is_action_pressed("right"):
+		position.x += speed * delta
