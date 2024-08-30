0e78d25cb1d8ef612a884717cee7d8dfeb9b3078
Sergei Babrakov <s.a.babrakov@gmail.com>
2024-08-30 08:54:05 +0200

    Spawn timer

--- scenes/game.gd
+++ scenes/game.gd
@@ -9,7 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
 
-func _on_gem_off_screen() -> void:
-	print("gem off the screen")
+func _on_timer_timeout() -> void:
+	print("_on_timer_timeout")
