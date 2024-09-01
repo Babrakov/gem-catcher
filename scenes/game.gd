82d398e9893d7764ec1dd8c1d1d0c569182a075e
Sergei Babrakov <s.a.babrakov@gmail.com>
2024-09-01 21:58:17 +0200

    Sounds

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+const EXPLODE = preload("res://assets/explode.wav")
+
 @export var gem_scene: PackedScene
-@onready var label: Label = $Label
+@onready var label: Label = $ScoreLabel
 @onready var timer: Timer = $Timer
 @onready var game_over_label: Label = $GameOverLabel
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 var _score: int = 0
 
@@ -31,15 +35,24 @@ func stop_all() -> void:
 		child.set_process(false)
 	game_over_label.text = "Game Over"
 	game_over_label.visible = true
+	
+func play_dead() -> void:	
+	audio_stream_player_2d.stop()
+	audio_stream_player_2d.stream = EXPLODE
+	audio_stream_player_2d.play()
+	audio_stream_player.stop()
 
 func game_over() -> void:
 	stop_all()
+	play_dead()
 
 func _on_timer_timeout() -> void:
 	spawn_gem()
 
 
-func _on_paddle_area_entered(area: Area2D) -> void:
+func _on_paddle_area_entered(area: Area2D) -> void:	
 	_score += 1
 	label.text = "%05d" % _score
+	audio_stream_player_2d.position = area.position
+	audio_stream_player_2d.play()
 	area.queue_free()
