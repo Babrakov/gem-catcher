6f786a229d662ab03acf42e9c61e8d0abe79510d
Sergei Babrakov <s.a.babrakov@gmail.com>
2024-09-01 21:06:11 +0200

    Spawning gems

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@export var gem_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	spawn_gem()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,6 +12,16 @@ func _process(delta: float) -> void:
 	pass
 	
 
+func spawn_gem() -> void:
+	var new_scene: Gem = gem_scene.instantiate()
+	var gem_size = 70
+	var xpos: float = randf_range(gem_size,get_viewport_rect().size.x-gem_size)
+	new_scene.on_gem_off_screen.connect(game_over)
+	new_scene.position = Vector2(xpos,-50)	
+	add_child(new_scene)
+
+func game_over() -> void:
+	print("Game Over")
 
 func _on_timer_timeout() -> void:
-	print("_on_timer_timeout")
+	spawn_gem()
--- scenes/gem.gd
+++ scenes/gem.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
+class_name Gem
+
 @export var speed: float = 100.0
 
 signal on_gem_off_screen
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#position.x = randi_range(0,get_viewport_rect().size.x)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
